a3b23ebcc5391877bf2de621972a1facea01f370
Saleeq Mohammed <saleeqvm@gmail.com>
2022-09-26 19:23:08 +0530

    updates

--- index.d.ts
+++ index.d.ts
@@ -1,12 +1,23 @@
-declare module 'react-draggable-tags' {
-    import * as React from 'react';
+declare module "react-draggable-tags" {
+  import * as React from "react";
 
-    export interface DraggableProps<T> {
-        tags: Array<T>;
-        render: (arg: { tag: T; index: number }) => JSX.Element;
-        onChange?: (tags: Array<T>) => void;
-        isList?: boolean;
-    }
+  export interface DraggableProps<T> {
+    tags: Array<T>;
+    render: (arg: { tag: T; index: number }) => JSX.Element;
+    onChange?: (tags: Array<T>) => void;
+    isList?: boolean;
+  }
 
-    export class DraggableArea<T> extends React.Component<DraggableProps<T>, unknown> { }
+  export interface DraggableAreasGroupProps<T> {
+    addArea: (areaId: any) => DraggableArea;
+  }
+
+  export class DraggableArea<T> extends React.Component<
+    DraggableProps<T>,
+    unknown
+  > {}
+  export class DraggableAreasGroup<T> extends React.Component<
+    DraggableAreasGroupProps<T>,
+    unknown
+  > {}
 }
